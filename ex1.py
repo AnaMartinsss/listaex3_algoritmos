e0b81e9d2de58015a3c73105841f9a6b10684d25
AnaMartinsss <martinsanalaura108@gmail.com>
2025-02-03 10:14:42 -0300

    att. ex1

--- ex1.py
+++ ex1.py
@@ -7,7 +7,17 @@ Exemplo de entrada: [1, 2, Exemplo de saída: [2, 4, 6, 8]
 
 '''
 
+'''
 lista_numeros = [randint(50,8000) for _ in range(50)]
 resultado = list(map(lambda x: x * 2, lista_numeros ))
 print(lista_numeros)
+print(resultado)
+'''
+
+lista = [randint(50,8000) for _ in range(50)]
+
+def dobrar_elementos(lista : list[int])-> list[int]:
+        return list(map(lambda x: x * 2,lista)) 
+
+resultado = dobrar_elementos(lista)
 print(resultado)
